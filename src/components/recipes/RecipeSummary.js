af39ef4b4b856539eb2192a842188b976d236541
justyna13 <justynal13.97@gmail.com>
2020-04-03 21:20:19 +0200

    added edit & delete buttons & added modal on click

--- src/components/recipes/RecipeSummary.js
+++ src/components/recipes/RecipeSummary.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import moment from "moment";
+import Modal from 'react-bootstrap/Modal';
 
 const RecipeSummary = ({recipe}) => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     if (recipe.createdAt) {
         return (
             <div className="recipe__summary">
 
                 <div className="recipe__title recipe__summary--title">
-                    <h4>{recipe.name}</h4>
+                    <h4 className="m-auto">{recipe.name}</h4>
+                    <button className="btn circle" onClick={handleShow}><i className="fa fa-trash fa-lg"> </i></button>
+                    <button className="btn circle"><i className="fa fa-edit fa-lg"> </i></button>
+
                 </div>
 
                 <div className="recipes__list--details">
@@ -23,6 +32,13 @@ const RecipeSummary = ({recipe}) => {
                     <h5>Added:</h5>
                     <p>{moment(recipe.createdAt.toDate()).calendar()}</p>
                 </div>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal heading</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                </Modal>
             </div>
         )
     } else {
